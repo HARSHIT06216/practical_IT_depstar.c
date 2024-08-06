7041bc3dbe96c545a72e811cd99c9b4a2303d2ad
HARSHIT06216 <harshitbalar6@gmail.com>
2024-08-06 09:43:43 +0530

    Add files via upload

---
+++ p4.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+    float population=1441881744, illiteratemen,illiteratewomen,men,women,literacy;
+    men=(48.4/100)*population;
+    women=(51.6/100)*population;
+    literacy=(85.95/100)*population;
+    illiteratemen=(19.15/100)*men;
+    illiteratewomen=(37.16/100)*women;
+
+    printf("\n total literacy is : %f",literacy);
+
+    printf("\n total illiterate men is : %f",illiteratemen);
+    printf("\n total illiterate women is : %f",illiteratewomen);
+
+    printf("\n\n name: BALAR HARSHIT BHUPATBHAI");
+        printf("\n ID: 24TCE0VO");
+}
